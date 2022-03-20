e5da816dfb7fa43b90b3622095fdcc554dce8f97
Bihari123 <thakur.cs.tarun@gmail.com>
2022-03-20 15:35:23 +0530

    improved routing and middleware:

--- routes.go
+++ routes.go
@@ -16,7 +16,8 @@ func routes(app *config.AppConfig) http.Handler {
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-
+    mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
